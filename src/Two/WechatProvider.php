cb1ab2dbdbc7b3e20ea8707176d61b116bd37b76
sunwr <wrem@msn.cn>
2019-04-24 00:18:28 +0800

    Create WechatProvider.php

---
+++ src/Two/WechatProvider.php
@@ -0,0 +1,182 @@
+<?php
+
+namespace Laravel\Socialite\Two;
+
+class WechatProvider extends AbstractProvider implements ProviderInterface
+{
+    /**
+     * The base url of WeChat API.
+     *
+     * @var string
+     */
+    protected $baseUrl = 'https://api.weixin.qq.com/sns';
+    /**
+     * {@inheritdoc}.
+     */
+    protected $openId;
+    /**
+     * {@inheritdoc}.
+     */
+    protected $scopes = ['snsapi_login'];
+    /**
+     * Indicates if the session state should be utilized.
+     *
+     * @var bool
+     */
+    protected $stateless = true;
+    /**
+     * Return country code instead of country name.
+     *
+     * @var bool
+     */
+    protected $withCountryCode = false;
+    /**
+     * @var WeChatComponentInterface
+     */
+    protected $component;
+    /**
+     * Return country code instead of country name.
+     *
+     * @return $this
+     */
+    public function withCountryCode()
+    {
+        $this->withCountryCode = true;
+        return $this;
+    }
+    /**
+     * WeChat OpenPlatform 3rd component.
+     *
+     * @param WeChatComponentInterface $component
+     *
+     * @return $this
+     */
+    public function component(WeChatComponentInterface $component)
+    {
+        $this->scopes = ['snsapi_base'];
+        $this->component = $component;
+        return $this;
+    }
+    /**
+     * {@inheritdoc}.
+     */
+    public function getAccessToken($code)
+    {
+        $response = $this->getHttpClient()->get($this->getTokenUrl(), [
+            'headers' => ['Accept' => 'application/json'],
+            'query' => $this->getTokenFields($code),
+        ]);
+        return $this->parseAccessToken($response->getBody());
+    }
+    /**
+     * {@inheritdoc}.
+     */
+    protected function getAuthUrl($state)
+    {
+        $path = 'oauth2/authorize';
+        if (in_array('snsapi_login', $this->scopes)) {
+            $path = 'qrconnect';
+        }
+        return $this->buildAuthUrlFromBase("https://open.weixin.qq.com/connect/{$path}", $state);
+    }
+    /**
+     * {@inheritdoc}.
+     */
+    protected function buildAuthUrlFromBase($url, $state)
+    {
+        $query = http_build_query($this->getCodeFields($state), '', '&', $this->encodingType);
+        return $url.'?'.$query.'#wechat_redirect';
+    }
+    /**
+     * {@inheritdoc}.
+     */
+    protected function getCodeFields($state = null)
+    {
+        if ($this->component) {
+            $this->with(['component_appid' => $this->component->getAppId()]);
+        }
+        return array_merge([
+            'appid' => $this->clientId,
+            'redirect_uri' => $this->redirectUrl,
+            'response_type' => 'code',
+            'scope' => $this->formatScopes($this->scopes, $this->scopeSeparator),
+            'state' => $state ?: md5(time()),
+            'connect_redirect' => 1,
+        ], $this->parameters);
+    }
+    /**
+     * {@inheritdoc}.
+     */
+    protected function getTokenUrl()
+    {
+        if ($this->component) {
+            return $this->baseUrl.'/oauth2/component/access_token';
+        }
+        return $this->baseUrl.'/oauth2/access_token';
+    }
+    /**
+     * {@inheritdoc}.
+     */
+    protected function getUserByToken($token)
+    {
+        $scopes = explode(',', $token->getAttribute('scope', ''));
+        if (in_array('snsapi_base', $scopes)) {
+            return $token->toArray();
+        }
+        if (empty($token['openid'])) {
+            throw new InvalidArgumentException('openid of AccessToken is required.');
+        }
+        $language = $this->withCountryCode ? null : (isset($this->parameters['lang']) ? $this->parameters['lang'] : 'zh_CN');
+        $response = $this->getHttpClient()->get($this->baseUrl.'/userinfo', [
+            'query' => array_filter([
+                'access_token' => $token->getToken(),
+                'openid' => $token['openid'],
+                'lang' => $language,
+            ]),
+        ]);
+        return json_decode($response->getBody(), true);
+    }
+    /**
+     * {@inheritdoc}.
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return new User([
+            'id' => $this->arrayItem($user, 'openid'),
+            'name' => $this->arrayItem($user, 'nickname'),
+            'nickname' => $this->arrayItem($user, 'nickname'),
+            'avatar' => $this->arrayItem($user, 'headimgurl'),
+            'email' => null,
+        ]);
+    }
+    /**
+     * {@inheritdoc}.
+     */
+    protected function getTokenFields($code)
+    {
+        return array_filter([
+            'appid' => $this->clientId,
+            'secret' => $this->clientSecret,
+            'component_appid' => $this->component ? $this->component->getAppId() : null,
+            'component_access_token' => $this->component ? $this->component->getToken() : null,
+            'code' => $code,
+            'grant_type' => 'authorization_code',
+        ]);
+    }
+    /**
+     * Remove the fucking callback parentheses.
+     *
+     * @param mixed $response
+     *
+     * @return string
+     */
+    protected function removeCallback($response)
+    {
+        if (false !== strpos($response, 'callback')) {
+            $lpos = strpos($response, '(');
+            $rpos = strrpos($response, ')');
+            $response = substr($response, $lpos + 1, $rpos - $lpos - 1);
+        }
+        return $response;
+    }
+}
